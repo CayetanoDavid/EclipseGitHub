4d476b06892ae1762e84ba6f22cea26bd789499b
MacDavo <MacDavo@192.168.1.20>
2017-05-23 15:59:44 -0500

    primer commit local con los siguientes archivos: 2 java, 3 xml, 1 xhtml, 1 jar y 1 manifest

---
+++ primeTest/src/main/java/mx/com/dmc/mb/TestMB.java
@@ -0,0 +1,71 @@
+package mx.com.dmc.mb;
+
+import java.util.ArrayList;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+
+@ManagedBean(name="testMB")
+@ViewScoped
+public class TestMB {
+
+	private ArrayList<Usuario> lista;
+	private Usuario usuarioSeleccionado;
+	
+	private String firstname;
+	private String lastname;
+	
+	public TestMB(){
+		System.out.println("Constructor TestMB");
+		lista = new ArrayList<Usuario>();
+		Usuario usuario;
+		for(int i=0; i<10; i++){
+			usuario = new Usuario("usuario "+i,"apellido "+i);
+			lista.add(usuario);
+		}
+		
+		usuarioSeleccionado = new Usuario();
+	}
+	
+	@PostConstruct
+	public void postConstruct(){
+		System.out.println("@PostConstruct");
+	}
+	
+	public void verifFilaSeleccionada(){
+		System.out.println("fila seleccionada "+this.getUsuarioSeleccionado().getNombre());
+	}
+
+	public ArrayList<Usuario> getLista() {
+		return lista;
+	}
+
+	public void setLista(ArrayList<Usuario> lista) {
+		this.lista = lista;
+	}
+
+	public Usuario getUsuarioSeleccionado() {
+		return usuarioSeleccionado;
+	}
+
+	public void setUsuarioSeleccionado(Usuario usuarioSeleccionado) {
+		this.usuarioSeleccionado = usuarioSeleccionado;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+}
